cde4190311958674462bfa6ef0d2da8de59693b2
Wilfredo Gomez <143476949+ThePageGuy@users.noreply.github.com>
2024-05-29 02:54:37 +0000

    Basic Structure

---
+++ src/TodoApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TodoApp = () => {
+  return (
+    <div>
+      <h1>Todo App</h1>
+      {/* Todo App components will go here */}
+    </div>
+  );
+};
+
+export default TodoApp;
