c18dd320ed6c91977fa6931bf1d6d1ddd3f22be2
Wilfredo Gomez <143476949+ThePageGuy@users.noreply.github.com>
2024-05-29 03:01:15 +0000

    TodoList Component

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import TodoForm from './TodoForm';
+import TodoList from './TodoList';
 
 const TodoApp = () => {
   const [todos, setTodos] = useState([]);
@@ -8,12 +9,11 @@ const TodoApp = () => {
     setTodos([...todos, todo]);
   };
 
-  // Functions to add, remove, and update todos will go here
-
   return (
     <div>
       <h1>Todo App</h1>
       <TodoForm addTodo={addTodo} />
+      <TodoList todos={todos} />
     </div>
   );
 };
